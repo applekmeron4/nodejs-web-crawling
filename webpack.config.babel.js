ab1bd874fbab3ed581358a37fc5547caeaae1d47
ljy <>
2018-04-09 15:34:12 +0900

    setup package and webpack

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,26 +1,25 @@
-
 import path from 'path';
-import webpack from 'webpack';
-import UglifyJsPlugin from 'uglifyjs-webpack-plugin';
+import nodeExternals from 'webpack-node-externals';
+
+const externals = [
+  'firebase-admin',
+  'firebase-functions'
+];
 
 module.exports = {
+  target: 'node',
   entry: {
     app: './src/index.js'
   },
   output: {
     path: path.resolve('build'),
     filename: '[name].js',
+    libraryTarget: 'this' // <-- Important
   },
   plugins: [
-    new webpack.LoaderOptionsPlugin({
-      minimize: true,
-    }),        
-    new webpack.optimize.ModuleConcatenationPlugin(),
-    new UglifyJsPlugin({
-      sourceMap: true,
-    }),
   ],
   resolve: {
     modules: ['node_modules'],
   },
+  externals: [nodeExternals()] 
 };
